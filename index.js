e314aa448724510dcc3cfc65eb74a472da159c50
vikas-z-pinnacle <vikas.zilpe@pinnacle.in>
2023-12-28 18:24:47 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
